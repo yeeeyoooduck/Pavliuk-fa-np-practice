dd003fec4f93c8a95abe3a26f78948a647c24ef8
Андропов Владимир Викторович <vvandropov@fa.ru>
2024-02-26 13:28:05 +0000

    6

---
+++ 1.sh
@@ -0,0 +1,41 @@
+cd "pi22-1" &&
+  git submodule add -f https://github.com/Duoksi/Kos
+  git submodule add -f https://github.com/mikayra/Abl
+  git submodule add -f https://github.com/areaal/bon
+  git submodule add -f https://github.com/Con2023/Kug
+  git submodule add -f https://github.com/piccologatto01/Kob &&
+  git submodule add -f https://github.com/emelyanovaolesya/Eme
+  git submodule add -f https://github.com/YakimkinDaniil/Yak &&
+  git submodule add -f https://github.com/Ivlev2004/Ivlev &&
+  git submodule add -f https://github.com/NastyMishina/MAS &&
+  git submodule add -f https://github.com/Kami11144/Man &&
+  git submodule add -f https://github.com/Butcher044/Giz &&
+  git submodule add -f https://github.com/lilitanechay/Nec &&
+  git submodule add -f https://github.com/GrankoNastya/Gra &&
+  git submodule add -f https://github.com/IamHenryEvery/Gri &&
+  git submodule add -f https://github.com/NikitinAnton/Nik &&
+  git submodule add -f https://github.com/OlgaBochagina/boch &&
+  git submodule add -f https://github.com/k1llRe4l/Pech
+
+
+  pi22-2
+  git submodule add -f https://github.com/JuanTrunov/TRUN &&
+  git submodule add -f https://github.com/MirQsan/Ism &&
+  git submodule add -f https://github.com/vladermik/erm &&
+  git submodule add -f https://github.com/yeeeyoooduck/pavliuk &&
+  git submodule add -f https://github.com/Gidr0n/kly &&
+  git submodule add -f https://github.com/mariiachet/Chetyrkina &&
+  git submodule add -f https://github.com/10nesse/Mash &&
+  git submodule add -f https://github.com/TanyaBabkina/btd &&
+  git submodule add -f https://github.com/teenee0/OAI &&
+  git submodule add -f https://github.com/Alyona544/Mor &&
+  git submodule add -f https://github.com/Arsenoid/Aki.git &&
+  git submodule add -f https://github.com/Kami1ot/Git-ovchinnikovPA &&
+  git submodule add -f https://github.com/hukamobeta/kuk.git &&
+  git submodule add -f https://github.com/osetter/DZY &&
+  git submodule add -f https://github.com/NasNasNas7631/SER &&
+  git submodule add -f https://github.com/GodWar101/Sam &&
+  git submodule add -f https://github.com/Anastas15/Khva &&
+  git submodule add -f https://github.com/diploud72/chebykin.git &&
+  git submodule add -f https://github.com/Cpsuls/proz
+
